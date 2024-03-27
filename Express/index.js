bb0bd70b6466710612a872c8e70206d4cd8ac3ba
Dattatray Venkat Panchal <panchaldatta1508@gmail.com>
2024-03-27 15:20:20 +0530

    Leraned The Concept Of EJS And How To Render EJS

--- Express/index.js
+++ Express/index.js
@@ -1,7 +1,15 @@
 const express =  require("express");
 const app = express();
+const path = require('path');
  
 let port  = 7000;
+app.set("view engine","ejs");
+app.set('views',path.join(__dirname,"/views"))
+
+app.get('/home',(req,res)=>{
+    res.render("home");
+    
+});
 
 app.listen(port,()=>{
     console.log(`Listining on port ${port}` );
