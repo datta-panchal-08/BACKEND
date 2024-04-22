4315dbe2b0bf9f6ee5a907495b15fb16a5745178
Dattatray Panchal <panchaldatta1508@gmail.com>
2024-04-22 11:40:36 +0530

    Learned How To Insert Many Users In DataBase By Using insertMany function

---
+++ Leraning Mongoose/index.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+main().then(()=>{
+    console.log("Connection Successfull");
+})
+.catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+}
+const userSchema = new mongoose.Schema({
+  name : String,
+  email : String,
+  age : Number
+});
+
+const User = mongoose.model("User",userSchema);
+
+// const user2 = new User({
+//   name : "Param",
+//   email: "Param@gmail.com",
+//   age : 21
+// });
+
+// user2.save().then((res)=>{
+//   console.log(res);
+// }).catch((err)=>{
+//   console.log(err);
+// })
+
+User.insertMany([
+  {name : "Datta", age : 21 , email : "datta@gmail.com"},
+  {name : "Param",age : 21 , email : "param@gmail.com"},
+  {name  : "Ganesh",age  : 22 , email : "ganesh@gmail.com"}
+]).then((res)=>{
+  console.log(res);
+}).catch((err)=>{
+  console.log(err);
+})
